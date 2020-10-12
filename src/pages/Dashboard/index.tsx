160817f8e32d7da9acde412f18ac1e2b15096b63
Rogerio Lucio <rogeriolucio@Rogerios-MacBook-Air.local>
2020-10-12 16:15:07 -0300

    criaçao das telas de detalhes das issues

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -3,7 +3,7 @@ import { FiChevronRight } from 'react-icons/fi'
 
 import logoImg from '../../assets/logo.svg';
 import api from '../../services/api'
-import Repository from '../Repository';
+import { Link } from 'react-router-dom';
 
 import { Title, Form, Repositories, Error } from './styles';
 
@@ -80,7 +80,7 @@ const Dashboard:React.FC = () => {
 
             <Repositories>
                 {repositories.map(repository => (
-                    <a key={repository.full_name} href="teste">
+                    <Link key={repository.full_name} to={`repositories/${repository.full_name}`}>
                         <img
                             src={repository.owner.avatar_url}
                             alt={repository.owner.login}
@@ -90,7 +90,7 @@ const Dashboard:React.FC = () => {
                             <p>{repository.description}</p>
                         </div>
                         <FiChevronRight size={20}/>
-                    </a>
+                    </Link>
                 ))}
             </Repositories>
 
